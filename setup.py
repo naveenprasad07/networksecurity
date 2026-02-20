83bcc6f49331645ab3f4312c177e474f82fed715
Naveen Prasad <128902289+naveenprasad07@users.noreply.github.com>
2026-02-20 08:19:10 +0000

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,40 @@
+'''
+The setup..py is an essentail part of packaging and
+distributing Python projects. It is used by setuptools
+(or disutils in older python versions) to define the configuration
+of the project, such as it's metadata, dependencies, and more
+'''
+
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+    requirement_lst:List[str]=[]
+
+    """
+    This function will return list of requirements
+    """
+    try:
+        with open("requirements.txt","r") as file:
+            # Read lines from the file
+            lines = file.readlines()
+            ## Process each line
+            for line in lines:
+                requirement = line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!='-e .':
+                    requirement_lst.append(requirement) 
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version = "0.0.1",
+    author = "Naveen Prasad",
+    author_email="naveenacad89@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
